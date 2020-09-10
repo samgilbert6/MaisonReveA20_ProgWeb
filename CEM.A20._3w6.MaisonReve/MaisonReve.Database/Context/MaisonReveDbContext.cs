5f67e042beede92703ff046e1027fa8702b81c07
Samuel Gilbert <1932650@cegepmontpetit.ca>
2020-09-10 16:08:08 -0400

    Ajout du DbContext

---
+++ CEM.A20._3w6.MaisonReve/MaisonReve.Database/Context/MaisonReveDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+using MaisonReve.Database.Models;
+
+namespace MaisonReve.Database.Context
+{
+    class MaisonReveDbContext:DbContext
+    {
+        public DbSet<Building> Buildings { get; set; }
+    }
+}
